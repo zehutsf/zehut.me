87e9eb8f30cd667a12cb83b925b0abe7ac0dc2ba
Brian Kadar <briankadar@gmail.com>
2016-09-30 14:06:49 -0700

    Serve static build directory

--- server.js
+++ server.js
@@ -23,6 +23,12 @@ app.get('/api/events', async (req, rw) => {
   }
 });
 
+app.use(express.static(__dirname + '/client/build'));
+
+app.get("*", (req, rw) => {
+  rw.html(__dirname + 'client/build/indexh.tml');
+});
+
 app.listen(app.get('port'), () => {
   console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
 });
